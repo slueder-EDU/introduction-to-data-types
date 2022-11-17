d833d37c663713dc57cff0ed648bcddafcba7088
slueder-EDU <118475870+slueder-EDU@users.noreply.github.com>
2022-11-17 10:21:49 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.showString("This is a string")
+basic.showString("2+2")
+basic.showString("1 < 0")
+basic.showString("This is a number")
+basic.showNumber(2 + 2)
+basic.clearScreen()
+basic.showString("This is a booleen")
+basic.showString("" + ((1 as any) < (0 as any)))
+basic.showString("" + (false))
+basic.forever(function () {
+	
+})
